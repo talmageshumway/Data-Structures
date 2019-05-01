3c3652dd2fa6617374248c50c197d47d81fd8341
talmageshumway <50176639+talmageshumway@users.noreply.github.com>
2019-05-01 09:45:50 -0600

    Create AVLTree.h

---
+++ AVLTree.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Node.h"
+
+class AVLTree
+{
+public:
+	AVLTree();
+	~AVLTree();
+
+	void Insert(int data);
+	Node* Search(int data);
+	//void InorderTraversal();
+	void PreOrderTraversal();
+	void Print();
+	int Size();
+	Node * root;
+	Node* GetRoot() { return root; };
+
+	Node* Rebalance(Node* cursor);
+	Node* RotateLeft(Node* cursor);
+	Node* RotateRight(Node* cursor);
+
+private:
+	void DestructorHelper(Node* cursor);
+	Node* InsertHelper(Node* cursor, int data);
+	Node* SearchHelper(Node* cursor, int data);
+	//void InorderHelper(Node* cursor);
+	void PreOrderTraversalHelper(Node* cursor);
+	void PrintHelper(Node* cursor, string offset);
+	int SizeHelper(Node* cursor);
+};
+
